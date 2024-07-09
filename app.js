0fb2206fd82e3db9a170ab8ad9dc1b9e9fb68368
Alan Araujo <alanaraujosilveira6@gmail.com>
2024-07-09 19:57:23 -0300

    logica feita

---
+++ app.js
@@ -0,0 +1,41 @@
+function codificar(){
+   
+ 
+    let texto =  document.getElementById('decodado').value;
+
+    let decodificado = texto.split('');
+    let texto_codificado = "";
+
+    for (let num = 0; num < decodificado.length; num++){
+
+        if(decodificado[num] == "a"){
+            texto_codificado = texto_codificado+"ai";
+            } else if(decodificado[num] == "e"){
+                texto_codificado = texto_codificado+"enter";
+                } else if(decodificado[num] == "i"){
+                    texto_codificado = texto_codificado+"imes";
+                        } else if(decodificado[num] == "o"){
+                           texto_codificado = texto_codificado+"ober";
+                            } else if(decodificado[num] == "u"){
+                                texto_codificado = texto_codificado+"ufat";
+                                } else{
+                                    texto_codificado = texto_codificado+ decodificado[num];
+                                }
+
+    }
+    document.getElementById('codado').value = texto_codificado;
+
+}
+
+function decodificar(){
+   
+    let texto =  document.getElementById('codado').value;
+
+    let texto_decodificado = texto.replace(/ai/g, 'a');
+    texto_decodificado = texto_decodificado.replace(/enter/g, 'e');
+    texto_decodificado = texto_decodificado.replace(/imes/g, 'i');
+    texto_decodificado = texto_decodificado.replace(/ober/g, 'o');
+    texto_decodificado = texto_decodificado.replace(/ufat/g, 'u');
+
+    document.getElementById('decodado').value = texto_decodificado;
+}
